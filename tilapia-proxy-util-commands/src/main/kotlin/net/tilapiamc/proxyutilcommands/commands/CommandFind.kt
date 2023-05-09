8f7d68ec2aefc69854a66e9f80541c5f1f79a016
fan87 <fan87.TW@gmail.com>
2023-05-09 18:34:26 +0800

    Force join button
    Took 12 minutes

--- tilapia-proxy-util-commands/src/main/kotlin/net/tilapiamc/proxyutilcommands/commands/CommandFind.kt
+++ tilapia-proxy-util-commands/src/main/kotlin/net/tilapiamc/proxyutilcommands/commands/CommandFind.kt
@@ -1,5 +1,6 @@
 package net.tilapiamc.proxyutilcommands.commands
 
+import com.velocitypowered.api.proxy.Player
 import net.kyori.adventure.text.event.ClickEvent
 import net.kyori.adventure.text.event.HoverEvent
 import net.tilapiamc.proxyapi.command.*
@@ -14,8 +15,10 @@ class CommandFind: ProxyCommand("find", "查看玩家所在的小遊戲", false)
     val miniGame = getCommandLanguageKey("MINIGAME", "&a玩家正在小遊戲 %1\$s  (種類 %2\$s)")
     val lobby = getCommandLanguageKey("LOBBY", "&a玩家正在大廳 %1\$s  (種類 %2\$s)")
     val clickToTeleport = getCommandLanguageKey("CLICK_TO_TELEPORT", "&e[點我傳送]")
+    val clickToForceJoin = getCommandLanguageKey("CLICK_TO_FORCE_JOIN", "&e[點我強制加入]")
     val clickToSpectate = getCommandLanguageKey("CLICK_TO_SPECTATE", "&e[點我觀戰]")
     val execute = getCommandLanguageKey("EXECUTE", "&e點我傳送至該玩家所在的遊戲")
+    val forceJoin = getCommandLanguageKey("FORCE_JOIN", "&e點我強制加入該玩家所在的遊戲")
     val spectate = getCommandLanguageKey("SPECTATE", "&e點我觀戰玩家所在的遊戲")
 
     init {
@@ -29,8 +32,12 @@ class CommandFind: ProxyCommand("find", "查看玩家所在的小遊戲", false)
                 sender.sendMessage(getLanguageBundle()[lobby].format(game.shortGameId, game.lobbyType))
                 sender.sendMessage(getLanguageBundle()[clickToTeleport].toComponent()
                     .clickEvent(ClickEvent.runCommand("/send ${game.shortGameId}"))
-                    .hoverEvent(HoverEvent.showText(getLanguageBundle()[execute].toComponent())
-                ))
+                    .hoverEvent(HoverEvent.showText(getLanguageBundle()[execute].toComponent()))                    .append("    ".toComponent())
+                    .append((getLanguageBundle()[clickToForceJoin]).toComponent()
+                        .clickEvent(ClickEvent.runCommand("/send ${game.shortGameId} ${(sender as Player).username} force-join"))
+                        .hoverEvent(HoverEvent.showText(getLanguageBundle()[forceJoin].toComponent()))
+                    )
+                )
                 sender.sendMessage("")
             } else if (game is MiniGame) {
                 sender.sendMessage("")
@@ -42,6 +49,11 @@ class CommandFind: ProxyCommand("find", "查看玩家所在的小遊戲", false)
                     .append((getLanguageBundle()[clickToSpectate]).toComponent()
                         .clickEvent(ClickEvent.runCommand("/spectate ${game.shortGameId}"))
                         .hoverEvent(HoverEvent.showText(getLanguageBundle()[spectate].toComponent()))
+                    )
+                    .append("    ".toComponent())
+                    .append((getLanguageBundle()[clickToForceJoin]).toComponent()
+                        .clickEvent(ClickEvent.runCommand("/send ${game.shortGameId} ${(sender as Player).username} force-join"))
+                        .hoverEvent(HoverEvent.showText(getLanguageBundle()[forceJoin].toComponent()))
                     )
                 )
                 sender.sendMessage("")
